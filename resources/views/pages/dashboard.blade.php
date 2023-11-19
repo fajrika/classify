94b0f0cf2f596d4d099bd49d2dc6c1fa82a55b9f
fajrika <fajrika.dev@gmail.com>
2023-11-20 00:15:16 +0700

    Update dashboard.blade.php

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -2,13 +2,6 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-8 d-flex align-items-strech">
-            <div class="card w-100">
-                <div class="card-body">
-                    a
-                </div>
-            </div>
-        </div>
         <div class="col-lg-4">
             <div class="row">
                 <div class="col-lg-12">
